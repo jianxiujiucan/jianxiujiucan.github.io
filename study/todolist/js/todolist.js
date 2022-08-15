b8c208c19010c7d392bf8dd3404cf87927255d8f
ningwenjing <ningwenjing@xunlei.com>
2022-08-15 19:50:04 +0800

    更新html结构，重构js

--- study/todolist/js/todolist.js
+++ study/todolist/js/todolist.js
@@ -1,191 +1,446 @@
-const inputWrite = document.getElementById("inputWrite");
-const buttonAdd = document.getElementById("buttonAdd");
-const list = document.getElementById("list");
-const todoTips = document.getElementById("todoTips");
-const clearAll = document.getElementById("clearAll");
-const iconClear = document.getElementById("iconClear");
-const todoItem = document.getElementById("todoItem");
-const completedItem = document.getElementById("completedItem");
+(function () {
+  /**
+   * 输入框
+   */
+  const inputWriteDom = document.getElementById("inputWrite");
 
-/** 存储的数据 */
-let dataBase = [];
+  /**
+   * 添加按钮
+   */
+  const buttonAddDom = document.getElementById("buttonAdd");
 
-if (localStorage.getItem("localData") !== null) {
-  // 如果本地有数据，则取本地的数据
-  dataBase = JSON.parse(window.localStorage.getItem("localData"));
-}
+  /**
+   * 待办列表
+   */
+  const toDoListDom = document.getElementById("list");
 
-/** 初次渲染列表 */
-renderData(dataBase);
+  /**
+   * 待办提示
+   */
+  const todoTipsDom = document.getElementById("todoTips");
 
-/**
- * 渲染列表
- * @param {Array} data 传入的数据
- */
-function renderData(data) {
-  let elements = "";
+  /**
+   * 清除按钮
+   */
+  const clearAllButtonDom = document.getElementById("clearAll");
 
-  if (data.length === 0) {
-    list.innerHTML = `<li class="todo-list__item is-empty">There wasn't anything...</li>` ;
-  } else {
-    clearAll.style.display = "flex";
-    for (let i = 0; i < data.length; i++) {
-      elements += `<li data-index="${i}" class="todo-list__item"><input data-index="${i}" class="checkbox" type="checkbox" ${
-        data[i].completed ? "checked" : ""
-      } onclick="completeTask(this)"><div class="todo-list__content" data-index="${i}"><p class="todo-list__text ${
-        data[i].completed ? "is-completed" : ""
-      }">${data[i].content}</p><p class="todo-list__time">${transformTime(
-        data[i].time
-      )}</p></div>
-        <div class="todo-list__operate">
-          <span class="todo-list__button todo-list__button--edit" data-index="${i}" title="Edit" onclick="editTask(this)"><i class="icon-edit"></i></span>
-          <span class="todo-list__button todo-list__button--save" data-index="${i}" title="Save" onclick="saveTask(this)"><i  class="icon-save"></i></span>
-          <span class="todo-list__button todo-list__button--delete" data-index="${i}" title="Delete" onclick="deleteTask(this)"><i data-index="${i}"class="icon-delete"></i></span>
-        </div></li>`;
+  /**
+   * 清除icon
+   */
+  const iconClearDom = document.getElementById("iconClear");
+
+  /**
+   * 待办tab
+   */
+  const todoTabDom = document.getElementById("todoTabItem");
+
+  /**
+   * 已办tab
+   */
+  const completeTabDom = document.getElementById("completedTabItem");
+
+  /**
+   * tab的所有节点
+   */
+
+  const tabsItems = document.querySelectorAll('.todo-tabs__item')
+
+  /**
+   * 全部tab
+   */
+  const allTabDom = document.getElementById("allTabItem");
+
+  /**
+   * 列表状态
+   */
+  const todoTypeList = {
+    all: 0,
+    todo: 1,
+    completed: 2,
+  };
+
+  /**
+   * 日期转换
+   * @param {Number} time 时间
+   * @returns 返回格式：年/月/日 时:分:秒
+   * @example
+   *
+   * transformTime(1660543723967)
+   * // => 2022/08/25 15:30:06
+   */
+  function transformTime(time) {
+    const date = new Date(time);
+    return date.toLocaleString();
+  }
+
+  const todoApp = {
+    /**
+     * 待办数据
+     */
+    localTodoData: [],
+    todoType: todoTypeList.all,
+    getTodoDataList() {
+      if (localStorage.getItem("localTodoData") !== null) {
+        // 如果本地有数据，则取本地的数据
+        this.localTodoData = JSON.parse(localStorage.getItem("localTodoData"));
+      }
+      if (localStorage.getItem("todoType") !== null) {
+        // 如果本地有数据，则取本地的数据
+        this.todoType = Number(localStorage.getItem("todoType"));
+      }
+    },
+    /**
+     * 渲染列表
+     */
+    renderList() {
+      let todoList = "";
+      let todoListItem = "";
+      console.log(this.todoType)
+      switch (this.todoType) {
+        case todoTypeList.all:
+          todoList = this.localTodoData.sort(
+            (a, b) => a.isCompleted - b.isCompleted
+          );
+          break;
+        case todoTypeList.todo:
+          todoList = this.localTodoData.filter((item) => !item.isCompleted);
+          break;
+        case todoTypeList.completed:
+          todoList = this.localTodoData.filter((item) => item.isCompleted);
+          break;
+      }
+
+      if (todoList.length === 0) {
+        todoListItem = `<li class="todo-list__item is-empty">There wasn't anything...</li>`;
+      } else {
+        clearAllButtonDom.style.display = "flex";
+
+        for (let i = 0; i < todoList.length; i++) {
+          const isEdit = todoList[i].isEdit;
+          const isComplete = todoList[i].isCompleted;
+          const time = todoList[i].time;
+
+          const tempContent = isEdit
+            ? `<div class="todo-list__content">
+            <input class="input-edit" value=${todoList[i].content} />
+          </div>`
+            : `<div class="todo-list__content ${
+                isComplete ? "is-completed" : ""
+              }">
+            <p class="todo-list__text ">
+              ${todoList[i].content}
+            </p>
+            <p class="todo-list__time">
+              ${transformTime(time)}
+            </p>
+          </div>`;
+
+          const tempOperate = isEdit
+            ? `<div class="todo-list__operate">
+            <s class="todo-list__button icon-save" data-time="${time}" title="Save"></s>
+            <b class="todo-list__button icon-delete" data-time="${time}" title="Delete"></b>
+          </div>`
+            : `<div class="todo-list__operate">
+            <i class="todo-list__button icon-edit" data-time="${time}" title="Edit"></i>
+            <b class="todo-list__button icon-delete" data-time="${time}" title="Delete"></b>
+          </div>`;
+
+          const tempTodoItem = `
+          <li class="todo-list__item">
+            <input data-time="${time}" class="checkbox" type="checkbox" ${
+            isComplete ? "checked" : ""
+          }>
+            ${tempContent}
+            ${tempOperate}
+          </li>`;
+
+          todoListItem += tempTodoItem;
+        }
+      }
+
+      toDoListDom.innerHTML = todoListItem;
+    },
+
+    /**
+     * 清除todo输入框内容
+     */
+    clearInputWrite() {
+      inputWriteDom.value = "";
+    },
+
+    handleChangeAllTab() {
+      allTabDom.classList.add("is-active");
+      completeTabDom.classList.remove("is-active");
+      todoTabDom.classList.remove("is-active");
+
+      this.todoType = todoTypeList.all;
+      localStorage.setItem("todoType", this.todoType);
+
+      this.renderList();
+    },
+
+    /**
+     * 切换待办列表 TODO
+     */
+    handleChangeTodoTab() {
+      todoTabDom.classList.add("is-active");
+      completeTabDom.classList.remove("is-active");
+      allTabDom.classList.remove("is-active");
+
+      this.todoType = todoTypeList.todo;
+
+      localStorage.setItem("todoType", this.todoType);
+
+      this.renderList();
+    },
+
+    /**
+     * 切换已完成列表
+     */
+    handleChangeCompleteTab() {
+      completeTabDom.classList.add("is-active");
+      todoTabDom.classList.remove("is-active");
+      allTabDom.classList.remove("is-active");
+
+      this.todoType = todoTypeList.completed;
+
+      localStorage.setItem("todoType", this.todoType);
+
+      this.renderList();
+    },
+
+    /**
+     * 切换tab
+     */
+    handleChangeTab() {
+      todoTabDom.addEventListener("click", () => this.handleChangeTodoTab());
+
+      completeTabDom.addEventListener("click", () =>
+        this.handleChangeCompleteTab()
+      );
+
+      allTabDom.addEventListener("click", () => this.handleChangeAllTab());
+    },
+
+    /**
+     * 输入框获取焦点时，隐藏错误提示
+     */
+    hideTipsWhenFocus() {
+      inputWriteDom.addEventListener("focus", () => {
+        if (todoTipsDom.classList.contains("is-show")) {
+          todoTipsDom.classList.remove("is-show");
+        }
+      });
+    },
+    /**
+     * 切换清除图标显示隐藏
+     */
+    toggleClearIconWhenInput() {
+      inputWriteDom.addEventListener("input", () => {
+        if (inputWriteDom.value === "") {
+          iconClearDom.style.display = "none";
+        } else {
+          iconClearDom.style.display = "block";
+        }
+      });
+    },
+    /**
+     * 清除按钮点击时，清空文本框内容
+     */
+    handleClickClearIcon() {
+      iconClearDom.addEventListener("click", () => {
+        this.clearInputWrite();
+        iconClearDom.style.display = "none";
+      });
+    },
+    /**
+     * 添加按钮点击时，添加数据，并渲染列表
+     */
+    handleClickAdd() {
+      buttonAddDom.addEventListener("click", () => {
+        this.handleAddTask();
+      });
+    },
+
+    /**
+     * 按下回车键（包括108键盘的小键盘）时，添加数据，并渲染列表
+     */
+    handleEnterKeyAdd() {
+      document.body.addEventListener("keydown", (e) => {
+        if (e.code === "Enter" || e.code === "NumpadEnter") {
+          this.handleAddTask();
+        }
+      });
+    },
+
+    /**
+     * 添加数据，并渲染列表
+     */
+    handleAddTask() {
+      if (inputWriteDom.value.trim() === "") {
+        todoTipsDom.classList.add("is-show");
+      } else {
+        const addTodoItem = {
+          content: inputWriteDom.value,
+          isCompleted: false,
+          isEdit: false,
+          time: Date.now(),
+        };
+        this.localTodoData.unshift(addTodoItem);
+        localStorage.setItem(
+          "localTodoData",
+          JSON.stringify(this.localTodoData)
+        );
+
+        iconClearDom.style.display = "none";
+
+        this.renderList();
+
+        this.clearInputWrite();
+      }
+    },
+
+    /**
+     * 清除所有任务
+     */
+    handleClearAll() {
+      clearAllButtonDom.addEventListener("click", () => {
+        localStorage.clear();
+        this.localTodoData = [];
+
+        localStorage.setItem(
+          "localTodoData",
+          JSON.stringify(this.localTodoData)
+        );
+        this.renderList();
+      });
+    },
+
+    getCurTodoIndex(time) {
+      return this.localTodoData.findIndex((todo) => todo.time === time);
+    },
+
+    handleClickTodoItem() {
+      toDoListDom.addEventListener("click", (e) => {
+        const type = e.target.nodeName;
+        const time = Number(e.target.dataset.time);
+        const index = this.getCurTodoIndex(time);
+        switch (type) {
+          case "I":
+            this.editTask(index);
+            break;
+          case "S":
+            const content =
+              e.target.parentNode.parentNode.children[1].children[0].value;
+            this.saveTask(index, content);
+            break;
+          case "B":
+            this.deleteTask(index);
+            break;
+          case "INPUT":
+            time && this.completeTask(index);
+          default:
+            break;
+        }
+      });
+    },
+
+    /**
+     * 编辑任务
+     * @param {number} 索引值
+     */
+    editTask(index) {
+      this.localTodoData[index].isEdit = true;
+      this.renderList();
+    },
+
+    /**
+     * 保存任务
+     * @param {number} 索引值
+     * @param {string} 任务的内容
+     */
+    saveTask(index, content) {
+      const tempTodo = this.localTodoData[index];
+
+      this.localTodoData[index] = {
+        ...tempTodo,
+        isEdit: false,
+        content,
+        time: Date.now(),
+      };
+
+      localStorage.setItem("localTodoData", JSON.stringify(this.localTodoData));
+
+      this.renderList();
+    },
+
+    /**
+     * 删除当前任务
+     * @param {number} 索引值
+     */
+    deleteTask(index) {
+
+      this.localTodoData.splice(index, 1);
+
+      localStorage.setItem("localTodoData", JSON.stringify(this.localTodoData));
+
+      this.renderList();
+    },
+
+    /**
+     * 点击完成任务
+     * @param {number} 索引值
+     */
+    completeTask(index) {
+      this.localTodoData[index].isCompleted =
+        !this.localTodoData[index].isCompleted;
+
+      localStorage.setItem("localTodoData", JSON.stringify(this.localTodoData));
+
+      this.renderList();
+    },
+
+    /**
+     * 同步tab激活信息
+     * @param {number} index 
+     */
+    handleChageTab(index){
+      for(let i = 0; i< tabsItems.length; i++){
+        tabsItems[i].classList.remove('is-active')
+      }
+      tabsItems[index].classList.add('is-active')
+    },
+
+    bindListeners() {
+      this.handleChangeTab();
+      this.hideTipsWhenFocus();
+      this.toggleClearIconWhenInput();
+      this.handleClickClearIcon();
+      this.handleClickAdd();
+      this.handleClearAll();
+      this.handleClickTodoItem();
+      this.handleEnterKeyAdd();
+    },
+
+    render() {
+      this.getTodoDataList();
+      this.renderList();
+      this.bindListeners();
+    },
+  };
+
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "visible") {
+      todoApp.getTodoDataList();
+      todoApp.handleChageTab(Number(localStorage.getItem("todoType")))
+      todoApp.renderList();
     }
-    list.innerHTML = elements;
-  }
-}
+  });
 
-/**
- * 日期转换
- * @param {Number} time 
- * @returns 返回格式：年-月-日 时:分:秒
- * @example
- * 2022-08-25 15:30:06
- */
-function transformTime(time) {
-  const date = new Date(time);
-  Y = date.getFullYear() + "-";
-  M =
-    (date.getMonth() + 1 < 10
-      ? "0" + (date.getMonth() + 1)
-      : date.getMonth() + 1) + "-";
-  D = date.getDate() + " ";
-  h = date.getHours().toString().padStart(2, "0") + ":";
-  m = date.getMinutes().toString().padStart(2, "0") + ":";
-  s = date.getSeconds().toString().padStart(2, "0");
-  return Y + M + D + h + m + s;
-}
+  window.onload = () => {
+    this.todoType = todoTypeList.all;
+    localStorage.setItem("todoType", this.todoType);
+  };
 
-/**
- * 切换完整列表
- */
-todoItem.addEventListener("click", function () {
-  this.classList.add("is-active");
-  completedItem.classList.remove("is-active");
-  renderData(dataBase)
-});
-
-/**
- * 切换已完成列表
- */
-completedItem.addEventListener("click", function () {
-  this.classList.add("is-active");
-  todoItem.classList.remove("is-active");
-  let newarr = dataBase.slice()
-  renderData(newarr.filter((item) => item.completed === true))
-});
-
-/**
- * 输入框获取焦点时，隐藏错误提示
- */
-inputWrite.addEventListener("focus", () => {
-  if (todoTips.classList.contains("is-show")) {
-    todoTips.classList.remove("is-show");
-  }
-});
-
-
-/**
- * 添加输入框点击时，清除图标显示 
- */
-inputWrite.addEventListener("input", () => {
-  iconClear.style.display = "block";
-});
-
-/**
- * 清除按钮点击时，清空文本框内容
- */
-iconClear.addEventListener("click", function () {
-  inputWrite.value = "";
-  this.style.display = "none";
-});
-
-/**
- * 添加按钮点击时，添加数据，并渲染列表
- */
-buttonAdd.addEventListener("click", () => {
-  if (inputWrite.value === "") {
-    todoTips.classList.add("is-show");
-  } else {
-    dataBase.unshift({
-      content: inputWrite.value,
-      completed: false,
-      time: Date.now(),
-    });
-    localStorage.setItem("localData", JSON.stringify(dataBase));
-    renderData(dataBase);
-    inputWrite.value = "";
-    iconClear.style.display = "none";
-  }
-});
-
-/**
- * 编辑任务
- * @param {element} event 
- */
-function editTask(event) {
-  const contentItems = document.querySelectorAll(".todo-list__content");
-  const saveItems = document.querySelectorAll(".todo-list__button--save");
-  event.style.display = "none";
-  const index = event.dataset.index;
-  saveItems[index].style.display = "flex";
-  contentItems[
-    index
-  ].innerHTML = `<input class="input-edit" value=${dataBase[index].content} />`;
-}
-
-/**
- * 保存任务
- * @param {element} event 
- */
-function saveTask(event) {
-  const index = event.dataset.index;
-  const value = document.querySelector(".input-edit").value;
-  dataBase[index].content = value;
-  dataBase[index].time = Date.now();
-  localStorage.setItem("localData", JSON.stringify(dataBase));
-  renderData(dataBase);
-}
-
-/**
- * 删除当前任务
- * @param {element} event 
- */
-function deleteTask(event) {
-  const index = event.dataset.index;
-  dataBase.splice(index, 1);
-  localStorage.setItem("localData", JSON.stringify(dataBase));
-  renderData(dataBase);
-}
-
-/**
- * 点击完成任务
- * @param {*} event 
- */
-function completeTask(event) {
-  const index = event.dataset.index;
-  dataBase[index].completed = true;
-  localStorage.setItem("localData", JSON.stringify(dataBase));
-  renderData(dataBase);
-}
-
-/**
- * 清除所有任务
- */
-clearAll.addEventListener("click", () => {
-  localStorage.clear();
-  dataBase = [];
-  renderData(dataBase);
-});
+  todoApp.render();
+})();
